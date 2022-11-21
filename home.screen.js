f90484489bd84aa05d3a388d6c53fe35dc9648b9
SgtMeno <118101157+SgtMeno@users.noreply.github.com>
2022-11-21 19:33:45 +0800

    Add files via upload

---
+++ home.screen.js
@@ -0,0 +1,17 @@
+
+import react from "react";
+import { View } from "react-native";
+import { Title } from "react-native-paper";
+
+
+export const HomeScreen=()=>{
+
+    return(
+        <View>
+            <Title>
+                HomeScreen
+            </Title>
+        </View>
+
+    );
+}
